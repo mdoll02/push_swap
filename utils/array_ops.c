ebcc48c003d9e06a1a998db236e1cbda5649091a
mdoll <mdoll@student.42wolfsburg.de>
2023-03-08 14:11:07 +0100

    fix: removed CHECK_ARRAY macro

--- utils/array_ops.c
+++ utils/array_ops.c
@@ -6,7 +6,7 @@
 /*   By: mdoll <mdoll@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 09:49:10 by mdoll             #+#    #+#             */
-/*   Updated: 2023/03/08 14:08:26 by mdoll            ###   ########.fr       */
+/*   Updated: 2023/03/08 14:09:29 by mdoll            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,16 +40,6 @@ int	*build_array(char **argv, int argc, int *len)
 	return (ret);
 }
 
-#define CHECK_ARRAY(msg, array) \
-{ \
-	int index = 0;\
-	while (array[index])\
-	{\
-		printf(#msg": %d\n", array[index]);\
-		index++;\
-	}\
-}                               \//TODO: REMOVE
-
 void	bubble_sort(int *array, int len)
 {
 	int			i;
@@ -57,8 +47,6 @@ void	bubble_sort(int *array, int len)
 	int			temp;
 
 	i = 0;
-	printf("len: %d\n", len);
-	CHECK_ARRAY(before bubble sort, array)
 	while (i < len - 1)
 	{
 		j = i;
@@ -74,5 +62,4 @@ void	bubble_sort(int *array, int len)
 		}
 		i++;
 	}
-	CHECK_ARRAY(bubble sort, array)
 }
